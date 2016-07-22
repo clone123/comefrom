45333134218f1c104b603fd83ef57b2980421100
unknown <huanghuanhuan@ad1-zwt.corp.qihoo.net>
2016-07-22 17:56:12 +0800

    add comefrom

---
+++ comefrom.js
@@ -0,0 +1,9 @@
+/**
+ * Created by huanghuanhuan on 2016/7/22.
+ */
+
+module.exports =  {
+         init : function () {
+               console.log('hello , I am Huanghuan, from Beijing !');
+         }
+};
